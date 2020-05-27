72c614387bbbfd67814ad13411f35617c5b0422e
Tomasz <tomaszlipowicz@gmail.com>
2020-05-27 10:38:51 +0100

    email and full name step added with validation

--- src/components/steps/Email.js
+++ src/components/steps/Email.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Email() {
+  const { handleSubmit, register, errors } = useForm({ mode: 'onBlur' });
+  const onSubmit = (values) => console.log(values);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <fieldset className="wmnds-fe-fieldset">
+        <legend className="wmnds-fe-fieldset__legend">
+          <h3 className="wmnds-fe-question">What is your email address?</h3>
+          <p>We’ll automatically send disruption alerts to this address</p>
+        </legend>
+        {/* wmnds-fe-group--error */}
+        <div
+          className={`wmnds-fe-groupp ${
+            errors.Email ? 'wmnds-fe-group--error' : ''
+          }`}
+        >
+          <label className="wmnds-fe-label" htmlFor="FirstName">
+            Email address, for example name@example.com
+          </label>
+          <div className="wmnds-col-1 wmnds-col-sm-1-2">
+            <input
+              ref={register({
+                required: 'Enter yor email address',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'invalid email address',
+                },
+              })}
+              className={`wmnds-fe-input ${
+                errors.Email ? 'wmnds-fe-input--error' : ''
+              }`}
+              id="Email"
+              name="Email"
+              type="text"
+              inputMode="text"
+              spellCheck="false"
+            />
+            {errors.Email && (
+              <span className="wmnds-fe-error-message">
+                {errors.Email.message}
+              </span>
+            )}
+          </div>
+        </div>
+      </fieldset>
+      <button
+        type="button"
+        className="wmnds-btn wmnds-btn--disabled wmnds-col-1 wmnds-m-t-md"
+        onClick={handleSubmit(onSubmit)}
+      >
+        Continue
+      </button>
+    </>
+  );
 }
 
-export default Email
+export default Email;
--- src/components/steps/FullName.js
+++ src/components/steps/FullName.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const FullName = () => {
