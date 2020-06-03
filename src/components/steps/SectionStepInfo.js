def005e95a579fada5d4bceaf7fdda0027e84e0f
Tomasz <tomaszlipowicz@gmail.com>
2020-06-03 15:23:17 +0100

    add strong to the step

--- src/components/steps/SectionStepInfo.js
+++ src/components/steps/SectionStepInfo.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import style from './SectionStepInfo.module.scss';
 import PropTypes from 'prop-types';
+import style from './SectionStepInfo.module.scss';
 
 function SectionStepInfo(props) {
   const { section, description } = props;
   return (
     <div className={`${style.sectionWrapper}`}>
       <p className={`wmnds-m-b-xs ${style.section}`}>{section}</p>
-      <p className={`wmnds-m-b-sm ${style.description}`}>{description}</p>
+      <p className={`wmnds-m-b-sm ${style.description}`}>
+        <strong>{description}</strong>
+      </p>
     </div>
   );
 }
@@ -15,7 +17,6 @@ function SectionStepInfo(props) {
 SectionStepInfo.propTypes = {
   section: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-}
-
+};
 
 export default SectionStepInfo;
