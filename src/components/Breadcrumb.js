532c9af87274f7d688a91fe6e3a3a551e794ada9
Tomasz <tomaszlipowicz@gmail.com>
2020-06-03 11:49:33 +0100

    added bredcrumbs and conditional display

---
+++ src/components/Breadcrumb.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Breadcrumb() {
+  return (
+    <nav
+      aria-label="Breadcrumb"
+      className="wmnds-breadcrumb wmnds-breadcrumb--mobile-app wmnds-col-1"
+    >
+      <ol className="wmnds-breadcrumb__list">
+        <li className="wmnds-breadcrumb__list-item">
+          <a href="/" className="wmnds-breadcrumb__link">
+            Plan a journey
+          </a>
+        </li>
+        <li className="wmnds-breadcrumb__list-item">
+          <a href="#" className="wmnds-breadcrumb__link">
+            Disruptions
+          </a>
+        </li>
+        <li className="wmnds-breadcrumb__list-item">
+          <a
+            href="#"
+            className="wmnds-breadcrumb__link wmnds-breadcrumb__link--current"
+            aria-current="page"
+          >
+            Sign up to email alerts about disruption
+          </a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Breadcrumb;
