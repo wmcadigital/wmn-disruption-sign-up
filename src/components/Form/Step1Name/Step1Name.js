f393dc789f926e28c78109516d4f187683d3c855
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-05 11:16:28 +0100

    Convert step 1 to new form methods

--- src/components/Form/Step1Name/Step1Name.js
+++ src/components/Form/Step1Name/Step1Name.js
@@ -1,91 +1,63 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useRef } from 'react';
-import { useFormContext } from 'react-hook-form';
+// Import components
+import Input from 'components/shared/FormElements/Input/Input';
+import SectionStepInfo from '../../steps/SectionStepInfo';
 // Import custom hooks
 import useStepLogic from '../useStepLogic';
 
-import SectionStepInfo from '../../steps/SectionStepInfo';
-
 const Step1Name = () => {
-  const formRef = useRef();
-
+  const formRef = useRef(); // Used so we can keep track of the form DOM element
   const {
     register,
     handleSubmit,
     showGenericError,
     continueButton,
-  } = useStepLogic(formRef);
-  const { errors } = useFormContext();
+  } = useStepLogic(formRef); // Custom hook for handling continue button (validation, errors etc)
+
+  // Labels used on inputs and for validation
+  const fNameLabel = 'First name';
+  const lNameLabel = 'Last name';
+
+  // Logic used to validate fields
+  const fieldValidation = (name) => {
+    return register({ required: `${name} is required` });
+  };
+
   return (
     <form onSubmit={handleSubmit} ref={formRef} autoComplete="on">
+      {/* Subsection */}
       <SectionStepInfo section="Section 1 of 2" description="About you" />
+
+      {/* Show generic error message */}
       {showGenericError}
-      <fieldset className="wmnds-fe-fieldset wmnds-m-b-xl">
-        <legend className="wmnds-fe-fieldset__legend wmnds-col-1">
+
+      <fieldset className="wmnds-fe-fieldset">
+        <legend className="wmnds-fe-fieldset__legend">
           <h2>What is your name?</h2>
           <p>
             We’ll use this information to personalise your email
             <br /> notifications so they aren’t marked as spam.
           </p>
         </legend>
-        {/* wmnds-fe-group--error */}
-        <div className="wmnds-col-1 wmnds-col-md-2-5">
-          <div
-            className={`wmnds-fe-groupp ${
-              errors.FirstName ? 'wmnds-fe-group--error' : ''
-            }`}
-          >
-            <label className="wmnds-fe-label" htmlFor="FirstName">
-              First Name
-            </label>
-            {errors.FirstName && (
-              <span className="wmnds-fe-error-message">
-                Enter your first name 123
-              </span>
-            )}
-            <div className="wmnds-m-b-lg">
-              <input
-                ref={register({ required: true, maxLength: 20 })}
-                className={`wmnds-fe-input ${
-                  errors.FirstName ? 'wmnds-fe-input--error' : ''
-                }`}
-                id="FirstName"
-                name="FirstName"
-                type="text"
-                inputMode="text"
-                spellCheck="false"
-              />
-            </div>
-          </div>
-          <div
-            className={`wmnds-fe-groupp ${
-              errors.LastName ? 'wmnds-fe-group--error' : ''
-            }`}
-          >
-            <label className="wmnds-fe-label" htmlFor="LastName">
-              Last Name
-            </label>
-            {errors.LastName && (
-              <span className="wmnds-fe-error-message">
-                Enter your last name
-              </span>
-            )}
-            <div className="">
-              <input
-                ref={register({ required: true, maxLength: 20 })}
-                className={`wmnds-fe-input ${
-                  errors.LastName ? 'wmnds-fe-input--error' : ''
-                }`}
-                id="LastName"
-                name="LastName"
-                type="text"
-                inputMode="text"
-                spellCheck="false"
-              />
-            </div>
-          </div>
-        </div>
+
+        <Input
+          className="wmnds-col-1 wmnds-col-sm-2-3 wmnds-col-lg-1-2"
+          name="Firstname"
+          label={fNameLabel}
+          autocomplete="given-name"
+          fieldValidation={fieldValidation(fNameLabel)}
+        />
+        <Input
+          className="wmnds-col-1 wmnds-col-sm-2-3 wmnds-col-lg-1-2"
+          name="LastName"
+          label={lNameLabel}
+          autocomplete="family-name"
+          fieldValidation={fieldValidation(lNameLabel)}
+        />
       </fieldset>
+
+      {/* Continue button */}
       {continueButton}
     </form>
   );
