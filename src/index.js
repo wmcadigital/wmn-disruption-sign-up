9f1240a31e0da79abb8ab0f950acc66ec9d6e224
Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
2020-06-17 11:34:17 +0100

    Ds/sentry setup (#56)
    * Added sentry tracking
    
    * Added support for IE11

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn:
+    'https://b07ac88977e44a9a8b6c7dabf58fe190@o378798.ingest.sentry.io/5279827',
+});
 
 ReactDOM.render(
   <React.StrictMode>
