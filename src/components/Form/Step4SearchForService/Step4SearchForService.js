e271e13a33f817164258ab3651c9e2dffdffa971
Catia Costa <catia.rodrigues.costa@gmail.com>
2020-09-22 10:06:27 +0100

    Step 4: add cancel button to allow user to go back to step3 and cancel the bus addition.

--- src/components/Form/Step4SearchForService/Step4SearchForService.js
+++ src/components/Form/Step4SearchForService/Step4SearchForService.js
@@ -1,16 +1,26 @@
 /* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { FormDataContext } from '../../../globalState/FormDataContext';
 
 // Import components
 import Autocomplete from './autocomplete/Autocomplete';
 import SectionStepInfo from '../../shared/SectionStepInfo/SectionStepInfo';
+import Button from '../../shared/Button/Button';
 
 function Step4SearchForService() {
   const [mode, setMode] = useState('bus');
+  const [formDataState, formDataDispatch] = useContext(FormDataContext);
+
+  const getPreviousStep = incrementAmount => {
+    formDataDispatch({
+      type: 'UPDATE_STEP',
+      payload: formDataState.currentStep - incrementAmount
+    });
+  };
 
   return (
     <form
-      onSubmit={(e) => {
+      onSubmit={e => {
         e.preventDefault();
         return false;
       }}
@@ -19,12 +29,18 @@ function Step4SearchForService() {
         {/* Subsection */}
         <SectionStepInfo section="Section 2 of 2" description="Services" />
         <h2 className="">Add a service</h2>
-        <p className="wmnds-col-2-3">
-          You can sign up to 10 services at a time.
-        </p>
+        <p className="wmnds-col-2-3">You can sign up to 10 services at a time.</p>
         <p>You will receive an automatic email update for each disruption</p>
         <Autocomplete mode={mode} setMode={setMode} />
       </div>
+
+      <Button
+        btnClass="wmnds-btn wmnds-btn--destructive wmnds-col-1 wmnds-m-t-md"
+        text="Cancel"
+        onClick={() => {
+          getPreviousStep(1);
+        }}
+      />
     </form>
   );
 }
