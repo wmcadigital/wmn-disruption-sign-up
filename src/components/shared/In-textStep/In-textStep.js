4b8a01643fc0bda64bb22960456af5de3ba24625
Catia Costa <catia.rodrigues.costa@gmail.com>
2020-10-08 11:27:45 +0100

    Adding new components

---
+++ src/components/shared/In-textStep/In-textStep.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function IntextStep(props) {
+  const { steps } = props;
+
+  return (
+    <ol className="wmnds-in-text-step">
+      {steps.map((value, index) => {
+        return (
+          <li className="wmnds-in-text-step__item" key={index}>
+            {value}
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
+
+IntextStep.propTypes = {
+  steps: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default IntextStep;
