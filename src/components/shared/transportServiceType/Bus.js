923e23d5360d683514619ca04540b747e4f579c8
Catia Costa <catia.rodrigues.costa@gmail.com>
2020-10-26 15:35:13 +0000

    Update styling of Bus component (responsiveness).

--- src/components/shared/transportServiceType/Bus.js
+++ src/components/shared/transportServiceType/Bus.js
@@ -12,25 +12,27 @@ function Bus(props) {
     <>
       <div className="wmnds-grid wmnds-grid--justify-between wmnds-grid--align-center">
         {/* Left side (service number and route name) */}
-        <div className="wmnds-col-1 wmnds-col-lg-auto wmnds-grid wmnds-m-t-sm">
+        <div className="wmnds-col-1 wmnds-col-sm-3-5 wmnds-col-lg-3-5 wmnds-grid wmnds-m-t-sm wmnds-grid--align-center">
           <div className="wmnds-disruption-indicator-medium wmnds-m-r-md wmnds-col-auto">
             {serviceNumber}
           </div>
-          <div className="wmnds-col-2-3 wmnds-col-md-auto">
+          <div className="wmnds-col-2-3">
             <strong>{routeName}</strong>
           </div>
         </div>
         {/* Right side for remove service button */}
         {showRemove && (
-          <Button
-            btnClass={`wmnds-btn--destructive wmnds-col-1 wmnds-col-lg-auto wmnds-m-t-sm ${style.removeBtn}`}
-            text="Remove service"
-            iconRight="general-trash"
-            title={`Remove service ${serviceNumber}: ${routeName}`}
-            onClick={() => {
-              onClick();
-            }}
-          />
+          <div className="wmnds-col-1 wmnds-col-sm-2-5 wmnds-col-lg-auto wmnds-grid wmnds-m-t-sm wmnds-grid--align-center wmnds-grid--justify-end">
+            <Button
+              btnClass={`wmnds-btn--destructive wmnds-col-1 wmnds-col-lg-auto wmnds-m-t-sm ${style.removeRoute}`}
+              text="Remove service"
+              iconRight="general-trash"
+              title={`Remove service ${serviceNumber}: ${routeName}`}
+              onClick={() => {
+                onClick();
+              }}
+            />
+          </div>
         )}
       </div>
       <hr className="wmnds-col-1 wmnds-m-t-md wmnds-m-b-md" />
