3392ec253cc54bf1acf96deba7df7def43135600
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-11-03 11:13:27 +0000

    Updated service step

--- src/components/Form/Step7AddService/Step7AddService.js
+++ src/components/Form/Step7AddService/Step7AddService.js
@@ -28,7 +28,7 @@ function Step7AddService() {
     >
       <div>
         {/* Subsection */}
-        <SectionStepInfo section="Section 2 of 2" description="Services" />
+        <SectionStepInfo section="Section 1 of 2" description="Services" />
         <h2 className="wmnds-col-1 wmnds-col-lg-4-5">Add a service</h2>
         <p className="wmnds-col-1 wmnds-col-lg-4-5">
           We’ll send an automatic disruption alert for each service you add.
