fd8892d1f26ed75cc40be76569678d9edc7f5e5a
Gilad Grant <ggrant966@gmail.com>
2021-03-15 16:38:04 +0000

    add state to handle requesting recovery

--- src/components/Form/Step10SubmitConfirmation/Success.js
+++ src/components/Form/Step10SubmitConfirmation/Success.js
@@ -6,6 +6,7 @@ import { FormDataContext } from '../../../globalState/FormDataContext';
 function Success() {
   // eslint-disable-next-line no-unused-vars
   const [formDataState, formDataDispatch] = useContext(FormDataContext);
+  const { isRequestingRecovery } = formDataState;
   const { Phone, SMSAlert, EmailAlert, SMSTerms } = formDataState.formData;
 
   const alignCenter = {
@@ -16,7 +17,15 @@ function Success() {
   let steps;
   let title = 'Sign up to service disruption alerts';
 
-  if (Phone && (SMSAlert === 'yes' || SMSTerms) && EmailAlert === 'yes') {
+  if (isRequestingRecovery) {
+    /* For Account link recovery only */
+    title = 'Request a link to manage your disruption alerts';
+    message = 'We have sent the link to manage your disruption alerts to your email';
+    steps = [
+      'Visit the link in the email to manage your disruption alerts.',
+      'You can now manage your services and communication preferences. You can access the page at any time by visiting the link in your email.',
+    ];
+  } else if (Phone && (SMSAlert === 'yes' || SMSTerms) && EmailAlert === 'yes') {
     /* Text messages AND Email */
     message = 'You have successfully signed up to text message and email alerts';
     steps = [
@@ -40,14 +49,6 @@ function Success() {
     steps = [
       'We’ll send you an email asking to confirm your subscription.',
       'Once you have confirmed your subscription, you’ll receive disruption alerts to your email address.',
-    ];
-  } else {
-    /* For Account link recovery only */
-    title = 'Request a link to manage your disruption alerts';
-    message = 'We have sent the link to manage your disruption alerts to your email';
-    steps = [
-      'Visit the link in the email to manage your disruption alerts.',
-      'You can now manage your services and communication preferences. You can access the page at any time by visiting the link in your email.',
     ];
   }
 
